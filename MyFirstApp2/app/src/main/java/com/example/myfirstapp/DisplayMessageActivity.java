e17e18eaff7e4cc0f34aca45ed6090c5850c3d0e
Jason Catmull <jasoncatmull@gmail.com>
2017-05-19 12:45:34 -0600

    Completed assignment requirements

--- MyFirstApp2/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp2/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,13 +1,24 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    static final String PREFS_NAME = "com.example.myfirstapp_scriptures";
+
+    String message1;
+    String message2;
+    String message3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +29,33 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the strings
         Intent intent = getIntent();
-        String message1 = intent.getStringExtra(MainActivity.BOOK);
-        String message2 = intent.getStringExtra(MainActivity.CHAPTER);
-        String message3 = intent.getStringExtra(MainActivity.VERSE);
+        message1 = intent.getStringExtra(MainActivity.BOOK);
+        message2 = intent.getStringExtra(MainActivity.CHAPTER);
+        message3 = intent.getStringExtra(MainActivity.VERSE);
 
+        //proper formatting
         String finalMess = message1 + " " + message2 + ":" + message3;
+
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(finalMess);
+    }
+
+    public void saveScripture(View view) {
+        //save scripture to shared preferences
+        SharedPreferences prefs = getSharedPreferences("ID", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("BOOK", message1);
+        editor.putString("CHAPTER", message2);
+        editor.putString("VERSE", message3);
+        editor.commit();
+
+        //Toast letting user know that scripture was saved
+        Context context = getApplicationContext();
+        CharSequence text = "Scripture Saved";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
--- MyFirstApp2/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp2/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String BOOK = "com.example.myfirstapp.BOOK";
@@ -22,16 +25,38 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         Log.i("intent", "About to create intent with John 3:16");
         Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        //retrieve data from text fields
         EditText verse = (EditText) findViewById(R.id.editText);
         EditText book = (EditText) findViewById(R.id.editText2);
         EditText chapter = (EditText) findViewById(R.id.editText3);
+
+        //assign them to strings to manage format
         String message1 = verse.getText().toString();
         String message2 = book.getText().toString();
         String message3 = chapter.getText().toString();
 
+        //include them in our intent
         intent.putExtra(BOOK, message2);
         intent.putExtra(CHAPTER, message3);
         intent.putExtra(VERSE, message1);
-        startActivity(intent);    }
+        startActivity(intent);
+    }
 
+    public void loadScripture(View view) {
+        SharedPreferences mSharedPreference = getSharedPreferences("ID", 0);
+        String value = (mSharedPreference.getString("BOOK", ""));
+        String value2 = (mSharedPreference.getString("CHAPTER", ""));
+        String value3 = (mSharedPreference.getString("VERSE", ""));
+
+        //couldn't get this to work without declaring these all over again
+        EditText verse = (EditText) findViewById(R.id.editText);
+        EditText book = (EditText) findViewById(R.id.editText2);
+        EditText chapter = (EditText) findViewById(R.id.editText3);
+
+        //set our EditText objects with our saved values
+        verse.setText(value3);
+        book.setText(value);
+        chapter.setText(value2);
+    }
 }
